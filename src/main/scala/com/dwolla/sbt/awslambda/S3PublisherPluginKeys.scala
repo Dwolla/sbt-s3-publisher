78f054c9b0d5bae0d4b34c7bdb103fd0e7374b6e
Brian P. Holt <bholt@dwolla.com>
2018-01-08 11:48:22 -0600

    use the settingKey and taskKey macros instead of duplicating the label name

--- src/main/scala/com/dwolla/sbt/awslambda/S3PublisherPluginKeys.scala
+++ src/main/scala/com/dwolla/sbt/awslambda/S3PublisherPluginKeys.scala
@@ -6,10 +6,10 @@ import sbt._
 import scala.language.postfixOps
 
 trait S3PublisherPluginKeys {
-  lazy val s3BucketEnvironmentVariable = SettingKey[Option[String]]("environment variable possibly holding an override to «defaultS3Bucket»")
-  lazy val s3TransferManager = SettingKey[TransferManager]("S3 transfer manager")
-  lazy val defaultS3Bucket = SettingKey[String]("defaultS3Bucket", "Default S3 bucket for artifact")
-  lazy val s3Bucket = TaskKey[String]("s3Bucket", "S3 bucket for artifact. Uses «defaultS3Bucket», unless an environment variable overrides it")
-  lazy val s3Prefix = TaskKey[String]("s3Prefix", "S3 prefix")
-  lazy val s3Key = TaskKey[String]("s3Key", "S3 path in bucket")
+  lazy val s3BucketEnvironmentVariable = settingKey[Option[String]]("environment variable possibly holding an override to «defaultS3Bucket»")
+  lazy val s3TransferManager = settingKey[TransferManager]("S3 transfer manager")
+  lazy val defaultS3Bucket = settingKey[String]("Default S3 bucket for artifact")
+  lazy val s3Bucket = taskKey[String]("S3 bucket for artifact. Uses «defaultS3Bucket», unless an environment variable overrides it")
+  lazy val s3Prefix = taskKey[String]("S3 prefix")
+  lazy val s3Key = taskKey[String]("S3 path in bucket")
 }
