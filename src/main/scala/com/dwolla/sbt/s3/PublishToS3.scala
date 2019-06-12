749716cb5a35a1c741864b7e6b823da2e371680e
Charles Dunbar <cdunbar@dwolla.com>
2019-06-12 16:21:03 -0500

    Fix logic to deploy from master branch or ref of master branch

--- src/main/scala/com/dwolla/sbt/s3/PublishToS3.scala
+++ src/main/scala/com/dwolla/sbt/s3/PublishToS3.scala
@@ -3,6 +3,7 @@ package com.dwolla.sbt.s3
 import com.dwolla.sbt.s3.model.VersionedArtifact
 import com.typesafe.sbt.GitVersioning
 import com.typesafe.sbt.SbtGit.git
+import com.typesafe.sbt.git.GitRunner
 import sbt.Keys._
 import sbt._
 import sbtassembly.AssemblyPlugin
@@ -24,16 +25,24 @@ object PublishToS3 extends AutoPlugin {
     s3BucketEnvironmentVariable := plugin.defaultBucketEnvironmentVariable,
     s3TransferManager := plugin.s3TransferManager,
     defaultS3Bucket := plugin.defaultS3Bucket,
-    uploadedArtifact := (assembly in assembly).value
+    uploadedArtifact := (assembly in assembly).value,
+    publishBranch := "master"
   )
 
+  private def masterBranchIsCheckedOut(branch: String, headCommit: Option[String], gitRunner: GitRunner, workingDirectory: File, log: Logger): Boolean =
+    headCommit.contains(gitRunner(s"rev-parse $branch")(workingDirectory, log))
+
   lazy val tasks = Seq(
-    s3PublishSnapshot := !(git.gitCurrentBranch.value == "master" && !git.gitUncommittedChanges.value),
+    s3PublishSnapshot := !(
+      !git.gitUncommittedChanges.value && (
+        git.gitCurrentBranch.value == publishBranch.value ||
+          masterBranchIsCheckedOut(publishBranch.value, git.gitHeadCommit.value, git.runner.value, baseDirectory.value, streams.value.log)
+      )),
     s3Bucket := plugin.s3Bucket(s3BucketEnvironmentVariable.value, defaultS3Bucket.value),
     s3Prefix := plugin.s3Prefix(normalizedName.value, version.value, VersionedArtifact((assembly in assembly).value, s3PublishSnapshot.value)),
     s3Key := plugin.s3Key(s3Prefix.value, normalizedName.value),
     publish := plugin.publish(uploadedArtifact.value, s3Bucket.value, s3Key.value, streams.value.log, s3TransferManager.value)
-  )
+    )
 
   lazy val publishToS3Settings = Defaults.itSettings ++ defaultValues ++ tasks
 
